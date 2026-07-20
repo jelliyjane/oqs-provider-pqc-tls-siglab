88910827255acea27a218374ccf3bce7446f542d
jane kim <janekim@janeui-Macmini.local>
2026-07-20 16:15:49 +0900

    Enable SLH-DSA SHA2 and SHAKE TLS variants on OpenSSL 3.5

--- oqsprov/oqsprov.c
+++ oqsprov/oqsprov.c
@@ -1683,42 +1683,42 @@ int OQS_PROVIDER_ENTRYPOINT_NAME(const OSSL_CORE_HANDLE *handle,
 
 
 
-    if (strcmp("3.5.0", ossl_versionp) <= 0) {
+    if (0 && strcmp("3.5.0", ossl_versionp) <= 0) {
         rt_algo_filter_enabled = 1;
         sk_OPENSSL_STRING_push(rt_disabled_algs, "slhdsashake128s");
     }
 
 
 
-    if (strcmp("3.5.0", ossl_versionp) <= 0) {
+    if (0 && strcmp("3.5.0", ossl_versionp) <= 0) {
         rt_algo_filter_enabled = 1;
         sk_OPENSSL_STRING_push(rt_disabled_algs, "slhdsashake128f");
     }
 
 
 
-    if (strcmp("3.5.0", ossl_versionp) <= 0) {
+    if (0 && strcmp("3.5.0", ossl_versionp) <= 0) {
         rt_algo_filter_enabled = 1;
         sk_OPENSSL_STRING_push(rt_disabled_algs, "slhdsashake192s");
     }
 
 
 
-    if (strcmp("3.5.0", ossl_versionp) <= 0) {
+    if (0 && strcmp("3.5.0", ossl_versionp) <= 0) {
         rt_algo_filter_enabled = 1;
         sk_OPENSSL_STRING_push(rt_disabled_algs, "slhdsashake192f");
     }
 
 
 
-    if (strcmp("3.5.0", ossl_versionp) <= 0) {
+    if (0 && strcmp("3.5.0", ossl_versionp) <= 0) {
         rt_algo_filter_enabled = 1;
         sk_OPENSSL_STRING_push(rt_disabled_algs, "slhdsashake256s");
     }
 
 
 
-    if (strcmp("3.5.0", ossl_versionp) <= 0) {
+    if (0 && strcmp("3.5.0", ossl_versionp) <= 0) {
         rt_algo_filter_enabled = 1;
         sk_OPENSSL_STRING_push(rt_disabled_algs, "slhdsashake256f");
     }
